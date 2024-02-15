d6f0930dbffcff5feba6844b9e2970705cc8b1bd
flasky06 <bonnienjuguna106@gmail.com>
2024-02-15 09:13:24 +0300

    completed header,about and contact form

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { FaXTwitter } from "react-icons/fa6";
 import { CgMail } from "react-icons/cg";
 import Image from "next/image";
 import Link from "next/link";
+import ProjectCard from "@/components/ProjectCard";
 
 export default function Home() {
   return (
@@ -21,13 +22,13 @@ export default function Home() {
           functionality with a touch of innovation
         </p>
         <div className="flex gap-4">
-          <span className="h-16 w-16 border flex items-center justify-center rounded-full cursor-pointer ">
+          <span className="h-16 w-16 border flex items-center justify-center rounded-full cursor-pointer  hover:scale-110 hover:border-[#5651e5] ease-in duration-300 ">
             <FaLinkedin className="text-blue-700 text-2xl" />
           </span>
-          <span className="h-16 w-16 border flex items-center justify-center rounded-full cursor-pointer ">
-            <FaGithub className="text-2xl" />
+          <span className="h-16 w-16 border flex items-center justify-center rounded-full cursor-pointer  hover:scale-110 hover:border-[#5651e5] ease-in duration-300">
+            <FaGithub className="text-2xl hover:scale-110  ease-in duration-300" />
           </span>
-          <span className="h-16 w-16 border flex items-center justify-center rounded-full cursor-pointer ">
+          <span className="h-16 w-16 border flex items-center justify-center rounded-full cursor-pointer  hover:scale-110 hover:border-[#5651e5] ease-in duration-300">
             <CgMail className="text-red-500 text-2xl" />
           </span>
         </div>
@@ -36,7 +37,7 @@ export default function Home() {
         <h3 className="text-center text-xl tracking-widest uppercase text-blue-700">
           About Me
         </h3>
-        <h3 className="text-lg font-extralight">Who am i ?</h3>
+        <h3 className="text-lg font-extralight capitalize">Who am i ?</h3>
         <p className="text-md">
           I am a Software Developer,With over 2+ years of professional
           experience, I have a proven track record of delivering high-quality
@@ -49,7 +50,7 @@ export default function Home() {
           new teams while delivering the desired results.
         </p>
         <p
-          className="cursor-pointer py-1 px-2 text-blue-400 hover:text-blue-600 underline underline-offset-4
+          className="cursor-pointer py-1 px-2 text-slate-800 hover:text-blue-600 underline underline-offset-4
 "
         >
           Check out some of my latest projects.
@@ -176,7 +177,7 @@ export default function Home() {
             {/*  Mongodb*/}
             <div className="p-2 shadow rounded-xl  ease-in duration-300">
               <div className="flex flex-row gap-1 justify-center items-center">
-                <div className="relative  w-20 h-16">
+                <div className="relative  w-24 h-16">
                   <Image
                     src={"/skills/mongodb.png"}
                     className="absolute"
@@ -192,7 +193,7 @@ export default function Home() {
             {/* git  */}
             <div className="p-2 shadow rounded-xl  ease-in duration-300">
               <div className="flex flex-row gap-1 justify-center items-center">
-                <div className="relative  w-20 h-14">
+                <div className="relative  w-24 h-14">
                   <Image
                     src={"/skills/git.png"}
                     className="absolute"
@@ -208,10 +209,36 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="flex flex-col max-w-3xl mx-auto items-center justify-center gap-4 mt-6">
+      <section className="flex flex-col max-w-5xl mx-auto items-center justify-center gap-4 mt-6 ">
         <h3 className="text-center text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
         </h3>
+        <div className="grid grid-cols-3 gap-4 w-full">
+          <ProjectCard
+            title="gtc"
+            backgroundImg="/skills/postgress.png"
+            tech="next js"
+            projectUrl="www.gtc.com"
+          />
+          <ProjectCard
+            title="gtc"
+            backgroundImg="/skills/postgress.png"
+            tech="next js"
+            projectUrl="www.gtc.com"
+          />
+          <ProjectCard
+            title="gtc"
+            backgroundImg="/skills/postgress.png"
+            tech="next js"
+            projectUrl="www.gtc.com"
+          />{" "}
+          <ProjectCard
+            title="gtc"
+            backgroundImg="/skills/postgress.png"
+            tech="next js"
+            projectUrl="www.gtc.com"
+          />
+        </div>
       </section>
 
       <section className="flex flex-col max-w-7xl mx-auto  justify-center gap-8 mt-6">
@@ -219,8 +246,8 @@ export default function Home() {
           Contact Me
         </h3>
         <div className="flex lg:flex-row flex-col gap-4">
-          <div className=" w-1/3 flex flex-col items-center h-full border rounded-xl p-4">
-            <div className="lg:p-4 h-full ">
+          <div className=" w-1/3 flex flex-col items-center h-full border rounded-xl p-4 shadow">
+            <div className="lg:px-4 h-full ">
               <div className="relative w-32 h-32 rounded">
                 <Image
                   className="absolute rounded"
@@ -231,7 +258,9 @@ export default function Home() {
                 />
               </div>
               <div className="">
-                <h2 className="py-1 text-slate-950">Bonface Njuguna </h2>
+                <h2 className="py-1 text-slate-950 font-bold">
+                  Bonface Njuguna
+                </h2>
                 <p className="text-sm italic">Software Developer </p>
                 <p className="py-4 font-light">
                   I am available for freelance or full-time positions.
@@ -246,7 +275,7 @@ export default function Home() {
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <div className="rounded-full border p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <div className="rounded-full border p-3 cursor-pointer hover:scale-110 hover:border-[#5651e5] ease-in duration-300">
                       <FaGithub />
                     </div>
                   </Link>
@@ -255,7 +284,7 @@ export default function Home() {
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <div className="rounded-full border p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <div className="rounded-full border p-3 cursor-pointer hover:scale-110 hover:border-[#5651e5] ease-in duration-300">
                       <FaLinkedin className="text-blue-600" />
                     </div>
                   </Link>
@@ -264,7 +293,7 @@ export default function Home() {
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <div className="rounded-full border p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <div className="rounded-full border p-3 cursor-pointer hover:scale-110 hover:border-[#5651e5] ease-in duration-300">
                       <FaXTwitter />
                     </div>
                   </Link>
@@ -273,7 +302,7 @@ export default function Home() {
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <div className="rounded-full border p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <div className="rounded-full border p-3 cursor-pointer hover:scale-110 hover:border-[#5651e5] ease-in duration-300">
                       <CgMail className="text-red-500 text-xl" />
                     </div>
                   </Link>
@@ -295,6 +324,7 @@ export default function Home() {
                       className="border font-light text-sm rounded-lg px-2 py-1 flex border-gray-300"
                       type="text"
                       name="name"
+                      placeholder="Bonface Njuguna"
                     />
                   </div>
                   <div className="flex flex-col">
@@ -305,6 +335,7 @@ export default function Home() {
                       className="border font-light text-sm rounded-lg px-2 py-1 flex border-gray-300"
                       type="text"
                       name="phone"
+                      placeholder="+254717299106"
                     />
                   </div>
                 </div>
@@ -314,6 +345,7 @@ export default function Home() {
                     className="border font-light text-sm rounded-lg px-2 py-1 flex border-gray-300"
                     type="email"
                     name="email"
+                    placeholder="bonnienjuguna@gmail.com"
                   />
                 </div>
                 <div className="flex flex-col py-1">
@@ -322,6 +354,7 @@ export default function Home() {
                     className="border font-light text-sm rounded-lg px-2 py-1 flex border-gray-300"
                     type="text"
                     name="subject"
+                    placeholder="Reason for enquiry"
                   />
                 </div>
                 <div className="flex flex-col py-1">
@@ -330,6 +363,7 @@ export default function Home() {
                     className="border font-light text-sm rounded-lg px-2 py-1 border-gray-300"
                     rows="3"
                     name="message"
+                    placeholder="I am contacting you ..."
                   ></textarea>
                 </div>
                 <button className="w-1/2 flex justify-center rounded mx-auto px-3 py-2 text-gray-100 mt-4 bg-[#5651e5]">
@@ -340,6 +374,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <footer className="h-28">{/* <p>@copyright 2024</p> */}</footer>
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 function Header() {
   return (
-    <div className="flex justify-around items-center py-5 bg-white shadow top-0 z-10 sticky">
-      <div className="relative h-16 w-32 p-1 cursor-pointer">
+    <div className="flex justify-around items-center py-3 bg-white shadow top-0 z-10 sticky">
+      <div className="relative h-16 w-36 p-1 cursor-pointer">
         <Image src={"/images/logo.png"} className="absolute " alt="/" fill />
       </div>
       <ul className="flex gap-4 capitalize">
